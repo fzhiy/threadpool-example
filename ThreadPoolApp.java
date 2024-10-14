da89570a2067ad2318b624da9ed1454395a477a7
Feng <fy274@exeter.ac.uk>
2024-10-14 14:36:57 +0100

    Added the ThreadPoolApp file

---
+++ ThreadPoolApp.java
@@ -0,0 +1,39 @@
+// package threadpool_example;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+
+public class ThreadPoolApp {
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            ThreadPoolApp.error();
+        }
+        try {
+            int numberOfJobs = Integer.parseInt(args[0]);
+            int numberOfThreads = Integer.parseInt(args[1]);
+            System.out
+                    .println("Creating a pool of " + numberOfThreads + " threads to process " + numberOfJobs + " jobs");
+            if ((numberOfJobs < 1) || (numberOfThreads < 1)) {
+                ThreadPoolApp.error();
+            }
+            ExecutorService pool = Executors.newFixedThreadPool(numberOfThreads);
+
+            Job[] jobs = new Job[numberOfJobs];
+            for (int i = 0; i < numberOfJobs; i++) {
+                jobs[i] = new Job(i);
+                pool.execute(jobs[i]); // executes the command at future time
+            }
+            pool.shutdown(); // shutdown the pool. Previously submitted tasks are executed, but no new tasks
+                             // will be accepted.
+        } catch (NumberFormatException e) {
+            ThreadPoolApp.error();
+        }
+    }
+
+    private static void error() {
+        System.err.println("ThreadPoolApp must be run with two positive valued "
+                + "integer arguments. The first detailing the number of jobs "
+                + "the second the number of processing threads in the pool");
+        System.exit(0); // exit the program
+    }
+}
